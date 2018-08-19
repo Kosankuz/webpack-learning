e7845e329c23a7e69cc23a527f64c5159271f1f0
kosankuz <konstantinkuzmenko@icloud.com>
2018-08-19 20:13:59 +0100

    commit

---
+++ lesson4-source-map-debugger/webpack.config.js
@@ -0,0 +1,16 @@
+'use strict'
+var path = require('path');
+
+module.exports = {
+  entry: "./home",    //entry point - what to build
+  output: {
+    path: path.resolve(__dirname, ''),  //path where to safe
+    filename: "build.js" ,  // where to save or export
+    library: "home"  // added a libiriary in order to export home.js
+  },
+  watch: true,         //this additional config set auto build
+  watchOptions: {
+    aggregateTimeout: 100   // after this miliseconds webpack is building bundle after module is updated
+  },
+  devtool: "source-map"
+};
