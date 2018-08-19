cfe76a1a524a0c2c809e95f9e20b3748d4c0ed0b
kosankuz <konstantinkuzmenko@icloud.com>
2018-08-19 22:51:50 +0100

    lesson5

---
+++ lesson5-babel-changed-toES6.js/home.js
@@ -0,0 +1,5 @@
+import  welcome from './welcome';
+
+welcome("Home");
+
+export let imported = welcome;  //exporting welcome function'
---
+++ lesson5-babel-changed-toES6.js/webpack.config.js
@@ -0,0 +1,24 @@
+'use strict'
+var path = require('path');
+
+module.exports = {
+  entry: "./home",    //entry point - what to build
+  output: {
+    path: path.resolve(__dirname, ''),  //path where to safe
+    filename: "build.js" ,  // where to save or export
+    library: "home"  // added a libiriary in order to export home.js
+  },
+  watch: true,         //this additional config set auto build
+  watchOptions: {
+    aggregateTimeout: 100   // after this miliseconds webpack is building bundle after module is updated
+  },
+  devtool: "source-map",
+  module: {
+           loaders: [
+               {
+                   test: /\.js$/,
+                   loader: 'babel-loader'  // babel makes code of the page readeble for older browsers 
+               }
+           ]
+       },
+};
