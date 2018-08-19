962df84818a270f3fae702f86d0d09d59c54f8a8
kosankuz <konstantinkuzmenko@icloud.com>
2018-08-19 01:00:31 +0100

    commit

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: '',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
+};
+
+
+
+// https://generatewebpackconfig.netlify.com visit this site for config generator;
